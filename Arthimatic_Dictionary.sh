f29d52b0582b562057721a805eefc51c26ecb5d6
Nikhil0836 <nikhilbtech836@gmail.com>
2022-08-06 20:56:22 +0530

    Dictionary

---
+++ Arthimatic_Dictionary.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+echo "Enter Three Numbers"
+read a
+read b
+read c
+
+echo "Arithmetic Operation1: $(($a + $b * $c))"
+
+echo "Arithmetic Operation2: $(($a * $b + $c))"
+
+echo "Arithmetic Operation3: $(($c * $a / $b ))"
+
+echo "Arithmetic Operation2: $(($a % $b + $c))"
+
+res1=$(($a + $b * $c))
+res2=$(($a * $b + $c))
+res3=$(($c * $a / $b ))
+res4=$(($a % $b + $c))
+
+arithmeticDict[res1]=$res1
+arithmeticDict[res2]=$res2
+arithmeticDict[res3]=$res3
+arithmeticDict[res4]=$res4
+
+echo "Arithmetic Dictionary: ${arithmeticDict[@]}"
